55295012c4694e2c6b7daa770981444584ca379c
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-11-29 20:25:05 +0300

    Publish photo to Telegram channel.

--- bot.py
+++ bot.py
@@ -3,4 +3,5 @@ import telegram
 
 bot = telegram.Bot(token='5866680710:AAF8Xfd3EEfyjn0vCFeCEYE-XrgAyYUC9Ns')
 print(bot.get_me())
-bot.send_message(chat_id='@space_pictures_generator', text="Welcome to Space Pictures.")
+# bot.send_message(chat_id='@space_pictures_generator', text="Welcome to Space Pictures.")
+bot.send_photo(chat_id='@space_pictures_generator', photo=open('images/nasa_epic_1.png', 'rb'))
