9ef07976b79db435c2dfc6413d38d260f8fcd31a
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-01 12:34:50 +0300

    Update nasa_apod.py

--- nasa_apod.py
+++ nasa_apod.py
@@ -14,7 +14,7 @@ def nasa_apod(api_key, path):
                               params=parameters)
     response_for_json.raise_for_status()
     decoded_response = response_for_json.json()
-    for order, dictionary in enumerate(decoded_response):
+    for order, dictionary in enumerate(decoded_response, start=1):
         response_for_image = requests.get(dictionary['url'])
         response_for_image.raise_for_status()
         exp = expansion_extractor.expansion_extractor(dictionary['url'])
