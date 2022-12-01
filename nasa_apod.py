f871edcae6b9c9f3c4935d89202aadbe4d308fcf
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-01 10:45:46 +0300

    Update nasa_apod.py

--- nasa_apod.py
+++ nasa_apod.py
@@ -3,20 +3,20 @@ import requests
 import expansion_extractor
 
 
-def nasa_apod(api, path):
+def nasa_apod(api_key, path):
     if not os.path.exists(path):
         os.makedirs(path)
     parameters = {
-        'api_key': api,
+        'api_key': api_key,
         'count': 30
     }
-    response_1 = requests.get('https://api.nasa.gov/planetary/apod',
+    response_for_json = requests.get('https://api.nasa.gov/planetary/apod',
                               params=parameters)
-    response_1.raise_for_status()
-    decoded_response = response_1.json()
+    response_for_json.raise_for_status()
+    decoded_response = response_for_json.json()
     for order, dictionary in enumerate(decoded_response):
-        response_2 = requests.get(dictionary['url'])
-        response_2.raise_for_status()
+        response_for_image = requests.get(dictionary['url'])
+        response_for_image.raise_for_status()
         exp = expansion_extractor.expansion_extractor(dictionary['url'])
         with open(os.path.join(path, f'nasa_apod_{order + 1}{exp}'), 'wb') as file:
-            file.write(response_2.content)
+            file.write(response_for_image.content)
