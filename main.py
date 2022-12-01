ef6163268a223a1f7a2b28b3a3383577beebe692
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-01 10:48:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ directory = 'images'
 
 def main():
     load_dotenv()
-    api_epic = os.environ['API_EPIC']
-    api_apod = os.environ['API_APOD']
+    epic_api_key = os.environ['API_EPIC']
+    apod_api_key = os.environ['API_APOD']
     spacex_id = os.environ['SPACEX_ID']
     try:
-        nasa_epic.nasa_epic(api_epic, directory)
+        nasa_epic.nasa_epic(epic_api_key, directory)
         fetch_spacex_last_launch.fetch_spacex_last_launch(spacex_id, directory)
-        nasa_apod.nasa_apod(api_apod, directory)
+        nasa_apod.nasa_apod(apod_api_key, directory)
         bot.publish_to_bot()
     except requests.exceptions.HTTPError:
         sys.exit('Некорректный запрос')
