d475fa94f067b23bb50549c9757034e0ef8b53d5
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-01 13:40:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ def main():
     load_dotenv()
     epic_api_key = os.environ['EPIC_KEY']
     apod_api_key = os.environ['APOD_KEY']
-    spacex_id = os.environ['SPACEX_ID']
+    spacex_launch_id = os.environ['SPACEX_LAUNCH_ID']
     try:
         nasa_epic.nasa_epic(epic_api_key, directory)
-        fetch_spacex_last_launch.fetch_spacex_last_launch(spacex_id, directory)
+        fetch_spacex_last_launch.fetch_spacex_last_launch(spacex_launch_id, directory)
         nasa_apod.nasa_apod(apod_api_key, directory)
         bot.publish_to_bot()
     except requests.exceptions.HTTPError:
