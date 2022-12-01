8ea38a860e15a0dbbb348fa5d7a0501b6dc0176b
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-01 10:47:32 +0300

    Update nasa_epic.py

--- nasa_epic.py
+++ nasa_epic.py
@@ -4,26 +4,26 @@ import datetime
 import expansion_extractor
 
 
-def nasa_epic(api, path):
+def nasa_epic(api_key, path):
     if not os.path.exists(path):
         os.makedirs(path)
-    parameters_1 = {
-        'api_key': api,
+    parameters_for_json = {
+        'api_key': api_key,
     }
-    response_1 = requests.get('https://api.nasa.gov/EPIC/api/natural/images',
-                              params=parameters_1)
-    response_1.raise_for_status()
-    decoded_response = response_1.json()
+    response_for_json = requests.get('https://api.nasa.gov/EPIC/api/natural/images',
+                              params=parameters_for_json)
+    response_for_json.raise_for_status()
+    decoded_response = response_for_json.json()
     for order, dictionary in enumerate(decoded_response):
         date = (datetime.datetime.fromisoformat(dictionary['date']))
         image_name = dictionary['image']
-        parameters_2 = {
-            'api_key': api,
+        parameters_for_image = {
+            'api_key': api_key,
         }
-        response_2 = requests.get(
+        response_for_image = requests.get(
             f'https://api.nasa.gov/EPIC/archive/natural/{date.year}/{date.month}/{date.day}/png/{image_name}.png',
             params=parameters_2)
-        response_2.raise_for_status()
-        exp = expansion_extractor.expansion_extractor(response_2.url)
+        response_for_image.raise_for_status()
+        exp = expansion_extractor.expansion_extractor(response_for_image.url)
         with open(os.path.join(path, f'nasa_epic_{order + 1}{exp}'), 'wb') as file:
-            file.write(response_2.content)
+            file.write(response_for_image.content)
