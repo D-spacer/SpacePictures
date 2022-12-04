f4ec8d4d96a728baafe7ce644817cef93377e1c6
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-04 16:48:59 +0300

    Replaced link to chat

--- bot.py
+++ bot.py
@@ -4,9 +4,8 @@ import random
 import time
 
 
-def publish_to_bot(timeout=14400):
+def publish_to_bot(telegram_chat_id, timeout=14400):
     bot_token = os.environ['TG_BOT_TOKEN']
-    telegram_chat_id = os.environ['TG_CHANNEL_ID']
     bot = telegram.Bot(token=bot_token)
 
     *_, content = os.walk('images')
