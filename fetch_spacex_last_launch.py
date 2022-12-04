64afe743937e49b3f933dc12169437e2743017d1
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-04 17:02:00 +0300

    Changed the name of the function

--- fetch_spacex_last_launch.py
+++ fetch_spacex_last_launch.py
@@ -1,6 +1,6 @@
 import os
 import requests
-import expansion_extractor
+import extract_expansion
 
 
 def fetch_spacex_last_launch(id, path):
@@ -11,6 +11,6 @@ def fetch_spacex_last_launch(id, path):
     for order, link in enumerate(decoded_response['links']['flickr']['original'], start=1):
         response_for_image = requests.get(link)
         response_for_image.raise_for_status()
-        exp = expansion_extractor.expansion_extractor(link)
+        exp = extract_expansion.extract_expansion(link)
         with open(os.path.join(path, f'spacex_{order + 1}{exp}'), 'wb') as file:
             file.write(response_for_image.content)
