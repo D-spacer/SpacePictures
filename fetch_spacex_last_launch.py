cd200809cc423b080e727f2ca1f10e7d09c768d1
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-04 16:52:59 +0300

    Changed variable names to be more distinct

--- fetch_spacex_last_launch.py
+++ fetch_spacex_last_launch.py
@@ -5,12 +5,12 @@ import expansion_extractor
 
 def fetch_spacex_last_launch(id, path):
     os.makedirs(path, exist_ok=True)
-    response_1 = requests.get(f'https://api.spacexdata.com/v5/launches/{id}')
-    response_1.raise_for_status()
+    response_for_json = requests.get(f'https://api.spacexdata.com/v5/launches/{id}')
+    response_for_json.raise_for_status()
     decoded_response = response_1.json()
     for order, link in enumerate(decoded_response['links']['flickr']['original'], start=1):
-        response_2 = requests.get(link)
-        response_2.raise_for_status()
+        response_for_image = requests.get(link)
+        response_for_image.raise_for_status()
         exp = expansion_extractor.expansion_extractor(link)
         with open(os.path.join(path, f'spacex_{order + 1}{exp}'), 'wb') as file:
-            file.write(response_2.content)
+            file.write(response_for_image.content)
