254e67809ac893ba495d061a4f8ec7b603241270
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-04 16:56:51 +0300

    Changed variable name to comply with standards

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 import os
 
 
-directory = 'images'
+DIRECTORY = 'images'
 
 
 def main():
@@ -17,9 +17,9 @@ def main():
     apod_api_key = os.environ['APOD_KEY']
     spacex_launch_id = os.environ['SPACEX_LAUNCH_ID']
     try:
-        nasa_epic.nasa_epic(epic_api_key, directory)
-        fetch_spacex_last_launch.fetch_spacex_last_launch(spacex_launch_id, directory)
-        nasa_apod.nasa_apod(apod_api_key, directory)
+        nasa_epic.nasa_epic(epic_api_key, DIRECTORY)
+        fetch_spacex_last_launch.fetch_spacex_last_launch(spacex_launch_id, DIRECTORY)
+        nasa_apod.nasa_apod(apod_api_key, DIRECTORY)
         bot.publish_to_bot(telegram_chat_id='@space_pictures_generator')
     except requests.exceptions.HTTPError:
         sys.exit('Некорректный запрос')
