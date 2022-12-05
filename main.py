dd4d62d5a1a891c8f758a4c7c5e4d1808c110bb8
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-05 14:25:15 +0300

    Added parsing of core variables

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import fetch_spacex_last_launch
 import nasa_epic
 import nasa_apod
 import bot
+import argparse
 from dotenv import load_dotenv
 import os
 
@@ -16,11 +17,17 @@ def main():
     epic_api_key = os.environ['EPIC_KEY']
     apod_api_key = os.environ['APOD_KEY']
     spacex_launch_id = os.environ['SPACEX_LAUNCH_ID']
+    parser = argparse.ArgumentParser(description='keys for TG')
+    parser.add_argument('chat_link', type=str, help='Input chat link')
+    parser.add_argument('bot_id', type=str, help='Input bot id')
+    args = parser.parse_args()
     try:
+        tg_bot_id = args.bot_id
+        tg_chat_link = args.url
         fetch_nasa_epic_images.fetch_nasa_epic_images(epic_api_key, DIRECTORY)
         fetch_spacex_last_launch.fetch_spacex_last_launch(spacex_launch_id, DIRECTORY)
         fetch_nasa_apod_images.fetch_nasa_apod_images(apod_api_key, DIRECTORY)
-        bot.publish_to_bot('@space_pictures_generator', '5866680710:AAF8Xfd3EEfyjn0vCFeCEYE-XrgAyYUC9Ns')
+        bot.publish_to_bot(tg_chat_link, tg_bot_id)
     except requests.exceptions.HTTPError:
         sys.exit('Некорректный запрос')
 
