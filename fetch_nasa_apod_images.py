2fcde1d474892bd06ae1599bf1bc38964f14d0ab
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2022-12-05 14:15:38 +0300

    Changed the name of response variable

--- fetch_nasa_apod_images.py
+++ fetch_nasa_apod_images.py
@@ -9,10 +9,10 @@ def fetch_nasa_apod_images(api_key, path, pictures_number=30):
         'api_key': api_key,
         'count': pictures_number
     }
-    response_for_json = requests.get('https://api.nasa.gov/planetary/apod',
+    response = requests.get('https://api.nasa.gov/planetary/apod',
                               params=parameters)
-    response_for_json.raise_for_status()
-    decoded_response = response_for_json.json()
+    response.raise_for_status()
+    decoded_response = response.json()
     for order, dictionary in enumerate(decoded_response, start=1):
         response_for_image = requests.get(dictionary['url'])
         response_for_image.raise_for_status()
